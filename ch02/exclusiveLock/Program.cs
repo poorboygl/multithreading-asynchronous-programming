0eaf83ac57218cdb10c5a183d6bc530728112b72
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-09 19:03:54 +0700

    ch02-3 -Exclusive Lock

---
+++ ch02/exclusiveLock/Program.cs
@@ -0,0 +1,28 @@
+﻿int counter = 0;
+
+object counterLock = new object();
+
+Thread thread1 = new Thread(IncrementCounter);
+Thread thread2 = new Thread(IncrementCounter);
+Thread thread3 = new Thread(IncrementCounter);
+thread1.Start();
+thread2.Start();
+thread3.Start();
+
+thread1.Join();
+thread2.Join();
+thread3.Join();
+
+Console.WriteLine($"Final counter value is: {counter}");
+
+
+void IncrementCounter()
+{
+    for (int i = 0; i < 100000; i++)
+    {
+        lock (counterLock)
+        {
+            counter = counter + 1;
+        }
+    }
+}
