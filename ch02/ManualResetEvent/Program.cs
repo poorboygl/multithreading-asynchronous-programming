8bcd33c1b113996953df91b804d047de343cfe85
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-11 15:59:52 +0700

    ch02-11 -Use ManualResetEvent to release multiple threads

---
+++ ch02/ManualResetEvent/Program.cs
@@ -0,0 +1,25 @@
+﻿using ManualResetEventSlim manualResetEvent = new ManualResetEventSlim(false);
+
+Console.WriteLine("Press enter to release all threads...");
+
+for (int i = 1; i <= 3; i++)
+{
+    Thread thread = new Thread(Work);
+    thread.Name = $"Thread {i}";
+    thread.Start();
+}
+
+
+Console.ReadLine();
+
+manualResetEvent.Set();
+
+Console.ReadLine();
+
+void Work()
+{
+    Console.WriteLine($"{Thread.CurrentThread.Name} is waiting for the signal...");
+    manualResetEvent.Wait();
+    Thread.Sleep(1000);
+    Console.WriteLine($"{Thread.CurrentThread.Name} has been released.");
+}
