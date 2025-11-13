dda3933621a4ad9c6f4fec9e5058fa80b0c463a6
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-13 22:09:11 +0700

    ch03-7 -Exception handling in threads

---
+++ ch03/ExceptionHandling/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+List<Exception> exceptions = new List<Exception>();
+object lockExceptions = new object();
+
+Thread thread1 = new Thread(Work);
+Thread thread2 = new Thread(Work);
+
+thread1.Start();
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
+
+foreach (var ex in exceptions)
+{
+    Console.WriteLine(ex.Message);
+}
+
+void Work()
+{
+    try
+    {
+        throw new InvalidOperationException("An error occured. This is expected.");
+    }
+    catch (Exception ex)
+    {
+        lock (lockExceptions)
+        {
+            exceptions.Add(ex);
+        }
+    }
+
+}
