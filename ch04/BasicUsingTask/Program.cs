23ac897e20acc48a6b6a68390c6a823cfbde948a
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-13 22:15:48 +0700

    ch04-2 -Basic Syntax of using Task

---
+++ ch04/BasicUsingTask/Program.cs
@@ -0,0 +1,13 @@
+﻿//Thread thread = new Thread(Work);
+//thread.Start();
+
+//Task task = new Task(Work);
+//task.Start();
+
+var t = Task.Run(Work);
+Console.ReadLine();
+
+void Work()
+{
+    Console.WriteLine("I love Programming!");
+}
