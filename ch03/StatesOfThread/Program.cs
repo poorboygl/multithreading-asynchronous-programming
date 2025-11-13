1c7e878dde46b999b2ac79618f1c58884d2c99c8
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-13 11:05:22 +0700

    ch03-2 -States of a thread

---
+++ ch03/StatesOfThread/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Thread[] threads = new Thread[10];
+
+for (int i = 0; i < 10; i++)
+{
+    threads[i] = new Thread(Work);
+    threads[i].Name = $"Thread {i}";
+    Console.WriteLine($"{threads[i].Name}'s state is {threads[i].ThreadState}");
+    threads[i].Start();
+}
+
+Thread.Sleep(3000);
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{threads[i].Name}'s state is {threads[i].ThreadState}");
+}
+
+Thread.CurrentThread.Name = "Master thread";
+Work();
+
+Thread.Sleep(3000);
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{threads[i].Name}'s state is {threads[i].ThreadState}");
+}
+
+void Work()
+{
+    Console.WriteLine($"Thread {Thread.CurrentThread.Name} started working. The state is {Thread.CurrentThread.ThreadState}");
+    Thread.Sleep(10000);
+    Console.WriteLine($"Thread {Thread.CurrentThread.Name} finished working. The state is {Thread.CurrentThread.ThreadState}");
+}
