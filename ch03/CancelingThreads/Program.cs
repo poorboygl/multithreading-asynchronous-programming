30f70fc21ad8b572e95cd3ecbf921b3a5bc15145
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-13 22:05:37 +0700

    ch03-5 -Canceling a thread

---
+++ ch03/CancelingThreads/Program.cs
@@ -0,0 +1,33 @@
+﻿bool cancelThread = false;
+
+Thread thread = new Thread(Work);
+thread.Start();
+
+Console.WriteLine("To cancel, press 'c'");
+var input = Console.ReadLine();
+if (input == "c")
+{
+    cancelThread = true;
+}
+
+thread.Join();
+Console.ReadLine();
+
+void Work()
+{
+    Console.WriteLine("Started doing the work.");
+
+    for (int i = 0; i < 100000; i++)
+    {
+        if (cancelThread)
+        {
+            Console.WriteLine($"User requested cancellation at iteration: {i}");
+            break;
+        }
+
+        Thread.SpinWait(300000);
+    }
+
+    Console.WriteLine("Work is done.");
+
+}
