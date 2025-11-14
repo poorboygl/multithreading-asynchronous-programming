a116c80a10f5062bf3cd91cf29885e58e604f211
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-14 19:27:46 +0700

    ch04-13 -Task Synchronization

---
+++ ch04/TaskSynchronization/Program.cs
@@ -0,0 +1,60 @@
+﻿Queue<string?> requestQueue = new Queue<string?>();
+
+object queueLock = new object();
+using SemaphoreSlim semaphore = new SemaphoreSlim(initialCount: 3, maxCount: 3);
+
+// 2. Start the requests queue monitoring thread
+Task monitoringTask = new Task(MonitorQueue);
+monitoringTask.Start();
+
+// 1. Enqueue the requests
+Console.WriteLine("Server is running. Type 'exit' to stop.");
+while (true)
+{
+    string? input = Console.ReadLine();
+    if (input?.ToLower() == "exit")
+    {
+        break;
+    }
+
+    lock (queueLock)
+    {
+        requestQueue.Enqueue(input);
+    }
+
+}
+
+void MonitorQueue()
+{
+    while (true)
+    {
+        if (requestQueue.Count > 0)
+        {
+            string? input;
+            lock (queueLock)
+            {
+                input = requestQueue.Dequeue();
+            }
+            semaphore.Wait();
+            Task processingTask = new Task(() => ProcessInput(input));
+            processingTask.Start();
+        }
+        Thread.Sleep(100);
+    }
+}
+
+// 3. Processing the requests
+void ProcessInput(string? input)
+{
+    try
+    {
+        // Simulate processing time
+        Thread.Sleep(2000);
+        Console.WriteLine($"Processed input: {input}");
+    }
+    finally
+    {
+        var prevCount = semaphore.Release();
+        Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} released the semaphore. Previous count is: {prevCount}");
+    }
+}
