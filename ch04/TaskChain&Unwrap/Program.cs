34875d7a3fc52dca79fed7a83f6192dda4bd8810
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-14 19:19:37 +0700

    ch04-11 -Task Coninuation - Chain & Unwrap

---
+++ ch04/TaskChain&Unwrap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using var client = new HttpClient();
+var taskPokemonListJson = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon");
+var taskGetFirstPokemonUrl = taskPokemonListJson.ContinueWith(t =>
+{
+    var result = t.Result;
+    var doc = JsonDocument.Parse(result);
+    JsonElement root = doc.RootElement;
+    JsonElement results = root.GetProperty("results");
+    JsonElement firstPokemon = results[0];
+    return firstPokemon.GetProperty("url").ToString();
+});
+var taskGetFirstPokemonDetailsJson = taskGetFirstPokemonUrl.ContinueWith(t =>
+{
+    var result = t.Result;
+    return client.GetStringAsync(result);
+}).Unwrap();
+_ = taskGetFirstPokemonDetailsJson.ContinueWith(t =>
+    {
+        var result = t.Result;
+        var doc = JsonDocument.Parse(result);
+        JsonElement root = doc.RootElement;
+        Console.WriteLine($"Name: {root.GetProperty("name").ToString()}");
+        Console.WriteLine($"Weight: {root.GetProperty("weight").ToString()}");
+        Console.WriteLine($"Height: {root.GetProperty("height")}");
+    });
+Console.WriteLine("This is the end of the program.");
+Console.ReadLine();
