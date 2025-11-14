92876b18fc124b86a590e73d944096ec3cc6e69e
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-14 19:46:25 +0700

    ch05-2 -Basic syntax of Async and Await

---
+++ ch05/BasicSyntax/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Starting to do work.");
+        var data = await FetchDataAsync();
+        Console.WriteLine($"Data is fetched: {data}");
+
+        Console.WriteLine("Press enter to exit.");
+        Console.ReadLine();
+    }
+
+    static async Task<string> FetchDataAsync()
+    {
+        await Task.Delay(2000);
+
+        return "Complex data";
+    }
+
+}
