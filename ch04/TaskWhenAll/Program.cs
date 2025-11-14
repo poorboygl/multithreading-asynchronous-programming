639c577a3880955345e3b604d9dc690af70893be
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-14 19:15:37 +0700

    ch04-10 -Task Continuation - WhenAll

---
+++ ch04/TaskWhenAll/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+int SumSegment(int start, int end)
+{
+    int segmentSum = 0;
+    for (int i = start; i < end; i++)
+    {
+        Thread.Sleep(100);
+        segmentSum += array[i];
+    }
+
+    return segmentSum;
+}
+
+int numofThreads = 4;
+int segmentLength = array.Length / numofThreads;
+
+Task<int>[] tasks = new Task<int>[numofThreads];
+tasks[0] = Task.Run(() => { return SumSegment(0, segmentLength); });
+tasks[1] = Task.Run(() => { return SumSegment(segmentLength, 2 * segmentLength); });
+tasks[2] = Task.Run(() => { return SumSegment(2 * segmentLength, 3 * segmentLength); });
+tasks[3] = Task.Run(() => { return SumSegment(3 * segmentLength, array.Length); });
+
+_ = Task.WhenAll(tasks).ContinueWith(t =>
+    {
+        Console.WriteLine($"The summary is {t.Result.Sum()}");
+    });
+
+Console.WriteLine("This is the end of the program.");
+
+Console.ReadLine();
