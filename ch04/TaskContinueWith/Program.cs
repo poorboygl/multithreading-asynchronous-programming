a1338df3061badd0d870494043d3153c27d2bee7
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-14 10:58:27 +0700

    ch04-9 -Task Continuation ContinueWith

---
+++ ch04/TaskContinueWith/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+using var client = new HttpClient();
+var task = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon");
+_  = task.ContinueWith(t =>
+{
+    var result = t.Result;
+    var doc = JsonDocument.Parse(result);
+    JsonElement root = doc.RootElement;
+    JsonElement results = root.GetProperty("results");
+    JsonElement firstPokemon = results[0];
+
+    Console.WriteLine($"First pokemon name: {firstPokemon.GetProperty("name")}");
+    Console.WriteLine($"First pokemon url: {firstPokemon.GetProperty("url")}");
+});
+
+Console.WriteLine("This is the end of the program.");
+Console.ReadLine();
