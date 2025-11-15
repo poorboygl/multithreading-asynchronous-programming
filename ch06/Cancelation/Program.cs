6efd2d6de8439549b3166a32ba48205b9a2a5e34
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-15 23:32:04 +0700

    ch06-7 -Parallel Loops Cancelation

---
+++ ch06/Cancelation/Program.cs
@@ -0,0 +1,40 @@
+﻿using var cts = new CancellationTokenSource();
+var token = cts.Token;
+
+var task = Task.Run(Work, token);
+
+Console.WriteLine("To cancel, press 'c'");
+var input = Console.ReadLine();
+if (input == "c")
+{
+    cts.Cancel();
+}
+
+
+task.Wait();
+Console.WriteLine($"Task status is: {task.Status}");
+Console.ReadLine();
+
+void Work()
+{
+    Console.WriteLine("Started doing the work.");
+
+    var options = new ParallelOptions { CancellationToken = cts.Token };
+
+    try
+    {
+        Parallel.For(0, 100000, options, i =>
+        {
+            Console.WriteLine($"{DateTime.Now}");
+            Thread.SpinWait(30000000);
+        });
+    }
+    catch (AggregateException ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+
+
+    Console.WriteLine("Work is done.");
+
+}
