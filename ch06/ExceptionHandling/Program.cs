5f8bb1b808256cbb63bf15b25b25c4aea6b11213
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-15 23:25:37 +0700

    ch06-3 -Parallel Loops Exceptions

---
+++ ch06/ExceptionHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] array = Enumerable.Range(0, 100).ToArray();
+
+int sum = 0;
+object lockSum = new object();
+
+
+try
+{
+    Parallel.For(0, array.Length, (i, state) =>
+    {
+        lock (lockSum)
+        {
+            if (!state.IsExceptional)
+            {
+                if (i == 65)
+                    throw new InvalidOperationException("This is on purpose.");
+
+                sum += array[i];
+                Console.WriteLine($"Current task id: {Task.CurrentId}; Is thread pool thread: {Thread.CurrentThread.IsThreadPoolThread}");
+            }
+        }
+    });
+
+
+}
+catch (AggregateException ex)
+{
+    Console.WriteLine(ex);
+}
+
+Console.WriteLine($"The sum is {sum}");
+
+Console.ReadLine();
