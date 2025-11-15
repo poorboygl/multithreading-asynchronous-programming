16c27343a8f518fcc5fbc84bcba8b6f5ec3fcb0f
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-15 12:51:38 +0700

    ch05-3 -Which thread is used

---
+++ ch05/WhichThread/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine($"1. Main thread id:{Thread.CurrentThread.ManagedThreadId}");
+
+        Console.WriteLine("Starting to do work.");
+        var data = await FetchDataAsync();
+        Console.WriteLine($"Data is fetched: {data}");
+
+        Console.WriteLine($"2. Thread id:{Thread.CurrentThread.ManagedThreadId}");
+
+        Console.WriteLine("Press enter to exit.");
+        Console.ReadLine();
+    }
+
+    static async Task<string> FetchDataAsync()
+    {
+        Console.WriteLine($"3. Thread id:{Thread.CurrentThread.ManagedThreadId}");
+
+        await Task.Delay(2000);
+
+        Console.WriteLine($"4. Thread id:{Thread.CurrentThread.ManagedThreadId}");
+
+        return "Complex data";
+    }
+
+}
