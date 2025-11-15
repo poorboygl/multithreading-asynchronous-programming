818de57f6049319e3eceef92f269b015844a8ae7
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-15 23:30:20 +0700

    ch06-5 -Parallel Loops Break

---
+++ ch06/Break/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] array = Enumerable.Range(0, 100).ToArray();
+
+int sum = 0;
+object lockSum = new object();
+
+
+try
+{
+    Parallel.For(0, array.Length, (i, state) =>
+    {
+        lock (lockSum)
+        {
+            if (state.ShouldExitCurrentIteration && state.LowestBreakIteration < i)
+                return;
+
+
+            if (i == 65)
+                state.Break();
+
+            sum += array[i];
+            Console.WriteLine($"Current task id: {Task.CurrentId}; Is thread pool thread: {Thread.CurrentThread.IsThreadPoolThread}");
+
+        }
+    });
+
+
+}
+catch (AggregateException ex)
+{
+    Console.WriteLine(ex);
+}
+
+Console.WriteLine($"The sum is {sum}");
+
+Console.ReadLine();
