a7304801ae75a360d87b963c600bbc1456832114
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-15 23:33:58 +0700

    ch06-8 -Thread Local Storage

---
+++ ch06/ThreadLocalStorage/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = Enumerable.Range(1, 10).ToArray();
+
+int sum = 0;
+object lockSum = new object();
+
+Parallel.For(
+    0,
+    array.Length,
+    () => 0,
+    (i, state, tls) =>
+    {
+        tls += array[i];
+        return tls;
+    },
+    tls =>
+    {
+        lock (lockSum)
+        {
+            sum += tls;
+            Console.WriteLine($"The task id: {Task.CurrentId}");
+        }
+    }
+);
+
+Console.WriteLine($"The sum is {sum}");
+
+Console.ReadLine();
