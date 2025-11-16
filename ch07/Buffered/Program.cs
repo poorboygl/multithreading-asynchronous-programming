20d221b3df8683fcde76cb116234e28cbcfda2f4
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-16 15:06:39 +0700

    ch07-2 -PLINQ - Producer, consumer and buffer

---
+++ ch07/Buffered/Program.cs
@@ -0,0 +1,15 @@
+﻿var items = Enumerable.Range(1, 200);
+
+var evenNumbers = items.AsParallel().WithMergeOptions(ParallelMergeOptions.FullyBuffered).Where(x =>
+{
+    Console.WriteLine($"Processing number {x}; Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+    return (x % 2 == 0);
+});
+
+Console.WriteLine();
+//Console.WriteLine($"There are {evenNumbers.Count()} even numbers in the collection.");
+
+foreach (var item in evenNumbers)
+{
+    Console.WriteLine($"{item}: Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+}
